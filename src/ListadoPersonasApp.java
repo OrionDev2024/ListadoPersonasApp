21119296ca8a8a4c230b0ce83563ad2adf1e025d
William Lopez R <lopezrebellonw@gmail.com>
2024-11-25 23:26:01 -0500

    funciones de añadir persona, listar persona y salir de la aplicacion

--- src/ListadoPersonasApp.java
+++ src/ListadoPersonasApp.java
@@ -13,6 +13,11 @@ public class ListadoPersonasApp {
         var salir = false;
         while (!salir){
             mostrarMenu();
+            try{
+                salir = ejecutarOperacion(consola, personas);
+            }catch (Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
             System.out.println();
         }
     }
@@ -26,4 +31,46 @@ public class ListadoPersonasApp {
                 """);
         System.out.print("Proporciona la opción: ");
     }
+    private static boolean ejecutarOperacion(Scanner consola, List<Persona> personas){
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        //Revisamos la opcion proporcionada
+        switch (opcion) {
+            case 1 ->{ //agregar persona a la lista
+                agregarPersona(consola, personas);
+                System.out.println("La lista tiene: " + personas.size() + " elementos.");
+            }// fin de case 1
+            case 2 ->
+                listarPersonas(personas);
+            //fin de case 2
+            case 3 -> {
+                //salimmos de nuestro ciclo
+                System.out.println("Hasta pronto...");
+                salir = true;
+            }//fin de caso 3
+            default -> System.out.println("Opcion Incorrecta: " + opcion);
+        }//fin swicht
+        return salir;
+
+    }//fin de funcion ejecutarOperacion
+    //función para agregar personas a la lista
+    private static void agregarPersona(Scanner consola, List<Persona> personas){
+        System.out.print("Proporciona el nombre: ");
+        var nombre = consola.nextLine();
+        System.out.print("Proporciona el teléfono: ");
+        var tel = consola.nextLine();
+        System.out.print("Proporciona el email: ");
+        var email = consola.nextLine();
+        //Crear el objeto Persona
+        var persona = new Persona(nombre, tel, email);
+        //lo agregamos a la lista
+        personas.add(persona);
+    }// fin de función agregarPersona
+    //función listar personas
+    private static void listarPersonas(List<Persona> personas){
+        System.out.println("Listado de Personas: ");
+        //Mejora usando lambda y metodo de referencia
+        //personas.forEach((persona) -> System.out.println(persona));
+        personas.forEach(System.out::println);
+    }// fin de funcion listarPersonas
 }
